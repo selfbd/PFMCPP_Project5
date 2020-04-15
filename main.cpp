5f0fce594bcd9ffcae41bfdca5f1a6b3f9f2e90e
matkatmusic <matkatmusic@gmail.com>
2020-04-15 00:07:02 -0700

    added part4 instructions

--- main.cpp
+++ main.cpp
@@ -1,33 +1,43 @@
 /*
- Project 5: Part 3 / 4
- video: Chapter 3 Part 4: 
+ Project 5: Part 4 / 4
+ video: Chapter 3 Part 7
 
-Create a branch named Part3
-
- the 'new' keyword
-
- 1) add #include "LeakedObjectDetector.h" to main
+ Create a branch named Part4
  
- 3) Add 'JUCE_LEAK_DETECTOR(OwnerClass)' at the end of your UDTs.
+ Don't #include what you don't use
  
- 4) write the name of your class where it says "OwnerClass"
+ 1) Your task is to refactor your Chapter 3 Part 4 task into separate source and header files.
+         Add files via the pane on the left.
  
- 5) write wrapper classes for each type similar to how it was shown in the video
+ 2) Put all of your declarations for each class in .h files
+         One header file per class ( i.e. Raider.h for a class named "Raider" )
  
- 7) update main to use your wrapper classes, which have your UDTs as pointer member variables.
+ 3) Put all of your implementations in .cpp files.
+         one cpp file per class ( i.e. Raider.cpp for a class named "Raider" )
  
- 8) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
+ 4) Put all of your Wrapper classes in a single Wrappers.h file
+         if you implemented your wrapper class functions in-class, you'll need to move them to Wrappers.cpp
+ 
+ 5) NO IN-CLASS IMPLEMENTATION ALLOWED.
+         the only exception is the existing Atomic.h and LeakedObjectDetector.h
+ 
+ 6) for every .cpp file you have to make, insert it into the .replit file after 'main.cpp'.  Don't forget the spaces between file names.
+ If you need help with this step, send me a DM.
+ 
+ 7) click the [run] button.  Clear up any errors or warnings as best you can.
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
-
+ 
 Make a pull request after you make your first commit and pin the pull request link to our DM thread.
 
 send me a DM to check your pull request
 
  Wait for my code review.
  
- see here for an example: https://repl.it/@matkatmusic/ch3p04example
+ Remember, your Chapter 3 Part 4 task worked when it was all in one file. so you shouldn't need to edit any of it.  
+         just split it up into source files and provide the appropriate #include directives.
  */
+
 
 // ======================================================================
 
